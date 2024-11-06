d2238bdc631bf91e4ae10850f25967347512b9aa
Skolmeister <alexander.nickel@mailbox.org>
2024-11-06 16:29:26 +0100

    requirements fuer docker (installationspakete und modmapping

---
+++ tools/app/requirements.r
@@ -0,0 +1,17 @@
+install.packages(
+  c(
+    "readr",
+    "here",
+    "stringr",
+    "purrr",
+    "glue",
+    "rvest",
+    "tibble",
+    "dplyr",
+    "tidyr",
+    "lubridate",
+    "knitr"
+  )
+)
+
+q()
